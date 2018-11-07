3f89c0a247034b8179d3b03640440aa5cd270460
MacAttack79 <jmcintosh1979@gmail.com>
2018-11-06 19:48:08 -0500

    updated code to process order and give total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,24 +13,25 @@ var mysql = require('mysql'),
     if (err) throw err;
     // Console Log to verify connection to the databasse has been established
     // console.log('Connection Established')
-    displayProducts()
+    displayProducts()    
   })
 
   function displayProducts() {
-    var query = 'SELECT item_id, product_name, price FROM products';
+    var query = 'SELECT item_id, product_name, stock_quantity, price FROM products';
 
     connection.query(query, function(err, data) {
       // console.log(data)
 
       var table = new Table({
-        head: ['ID', 'Product', 'Price'],
-        colWidths: [5, 50, 10]
+        head: ['ID', 'Product', 'Qty', 'Price'],
+        colWidths: [5, 45, 5, 10]
       });
       
       for (var i = 0; i < data.length; i++) {
         var productsArray = [
                 data[i].item_id, 
                 data[i].product_name,
+                data[i].stock_quantity,
                 data[i].price
               ]
 
@@ -39,4 +40,64 @@ var mysql = require('mysql'),
       console.log(table.toString())
       }
     )
+    runQuestions()
   }
+
+  function runQuestions () {
+    
+    connection.query("SELECT * FROM products", function(err, data) {
+      // console.log(data)
+      inquirer
+        .prompt ([
+          {
+          name: 'choice',
+          type: 'input',
+          message: 'What is the ID of the product you would like to purchase?',
+          },
+          {
+          name: 'qty',
+          type: 'input',
+          message: 'How many units would you like to purchase?',
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true
+            } 
+              return false
+            }
+          }
+        ])
+        .then(function(answer) {
+          // console.log(answer.choice, answer.qty)
+        
+            var query = "SELECT * FROM products WHERE item_id = "
+
+            connection.query(query + answer.choice, function(err, data) {
+
+            var dbInfo = data[0]
+            if (parseInt(answer.qty) <= dbInfo.stock_quantity) {
+              var newQty = dbInfo.stock_quantity - parseInt(answer.qty)
+              console.log("\nYour order has been accepted\n")
+
+              console.log("\n" + newQty + "\n")
+              
+              connection.query('UPDATE products SET stock_quantity = ' + newQty + "WHERE item_id = " + answer.choice, function(err, response){
+                
+                var cost = answer.qty * dbInfo.price
+                console.log("The total cost of your order is $" + cost)
+
+                console.log(dbInfo.stock_quantity)
+                // displayProducts();
+
+              })
+            } else {
+              console.log("Sorry, we do not have enough product available at this time.")
+            }
+            
+
+
+            // console.log(cost)
+            // console.log (newQty)
+              
+      });
+    })
+  })}
